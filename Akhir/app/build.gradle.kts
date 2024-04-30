ab7178f0c696dec54aac04bf4c144d683655f12e
rhyonomleni <93315228+rhyonomleni@users.noreply.github.com>
2024-04-30 17:06:57 +0700

    Ubah nama

--- Akhir/app/build.gradle.kts
+++ Akhir/app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.dicoding.tugasakhir"
+        applicationId = "id.ac.ukdw.food_viewer"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
